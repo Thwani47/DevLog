9bbc3eb2913cea34e61025b8cc57cdae71cd2467
Thwani sithole <thulaniinnocent47@gmail.com>
2025-02-19 20:05:05 +0200

    added a dividier b/w complete and open todos

--- src/renderer/src/components/List/TodoList.tsx
+++ src/renderer/src/components/List/TodoList.tsx
@@ -18,47 +18,71 @@ export const TodoList = ({ className, todos, filter, ...props }: TodoListProps) 
   const weekStart = today.startOf('week')
   const weekEnd = today.endOf('week')
 
+  const filteredTodos = todos
+    .filter((todo) => {
+      const dueDate = dayjs(todo.dueDate).startOf('day')
+
+      if (filter === TODOS_TODAY) {
+        return dueDate.isSame(today, 'day')
+      } else if (filter === TODOS_WEEK) {
+        return dueDate.isBetween(weekStart, weekEnd, 'day', '[]')
+      } else if (filter === TODO_OVERDUE) {
+        return dueDate.isBefore(today, 'day') && !todo.completed
+      }
+
+      return true
+    })
+    .sort((a, b) => a.dueDate.getTime() - b.dueDate.getTime())
+
+  const completedTodos = filteredTodos.filter((todo) => todo.completed)
+  const uncompletedTodos = filteredTodos.filter((todo) => !todo.completed)
+
   return (
     <div className={className} {...props}>
       <div className="mt-4">
-        {todos
-          .filter((todo) => {
-            const dueDate = dayjs(todo.dueDate).startOf('day')
-
-            if (filter === TODOS_TODAY) {
-              return dueDate.isSame(today, 'day')
-            } else if (filter === TODOS_WEEK) {
-              return dueDate.isBetween(weekStart, weekEnd, 'day', '[]')
-            } else if (filter === TODO_OVERDUE) {
-              return dueDate.isBefore(today, 'day') && !todo.completed
-            }
-
-            return true
-          })
-          .sort((a, b) => a.dueDate.getTime() - b.dueDate.getTime())
-          .sort((a, b) => (a.completed === b.completed ? 0 : a.completed ? 1 : -1))
-          .map((todo) => {
-            const { text, color } = formatDueDate(todo.dueDate)
-            return (
-              <div
-                key={todo._id}
-                className="p-3 border border-zinc-500/50 rounded-lg flex mb-2 flex-row justify-between hover:transition-all hover:scale-105"
-              >
-                <div className="flex flex-row items-center space-x-2">
-                  <div
-                    className={`h-10 w-10 rounded-full border border-zinc-600/50 ${todo.completed ? 'bg-green-500/70' : 'bg-zinc-500/10'}`}
-                    onClick={() => editTodo({ ...todo, completed: !todo.completed })}
-                  ></div>
-                  <p className={todo.completed ? 'line-through' : ''}>{todo.title}</p>
-                </div>
+        {uncompletedTodos.map((todo) => {
+          const { text, color } = formatDueDate(todo.dueDate)
+          return (
+            <div
+              key={todo._id}
+              className="p-3 border border-zinc-500/50 rounded-lg flex mb-2 flex-row justify-between hover:transition-all hover:scale-105"
+            >
+              <div className="flex flex-row items-center space-x-2">
                 <div
-                  className={`flex items-center ${color} ${todo.completed ? 'line-through' : ''}`}
-                >
-                  {text}
-                </div>
+                  className={`h-10 w-10 rounded-full border border-zinc-600/50 ${todo.completed ? 'bg-green-500/70' : 'bg-zinc-500/10'}`}
+                  onClick={() => editTodo({ ...todo, completed: !todo.completed })}
+                ></div>
+                <p className={todo.completed ? 'line-through' : ''}>{todo.title}</p>
               </div>
-            )
-          })}
+              <div className={`flex items-center ${color} ${todo.completed ? 'line-through' : ''}`}>
+                {text}
+              </div>
+            </div>
+          )
+        })}
+      </div>
+      {uncompletedTodos.length > 0 && <hr className={`mt-2 mb-2 border-t border-zinc-500`} />}
+      <div className="mt-4">
+        {completedTodos.map((todo) => {
+          const { text, color } = formatDueDate(todo.dueDate)
+          return (
+            <div
+              key={todo._id}
+              className="p-3 border border-zinc-500/50 rounded-lg flex mb-2 flex-row justify-between hover:transition-all hover:scale-105"
+            >
+              <div className="flex flex-row items-center space-x-2">
+                <div
+                  className={`h-10 w-10 rounded-full border border-zinc-600/50 ${todo.completed ? 'bg-green-500/70' : 'bg-zinc-500/10'}`}
+                  onClick={() => editTodo({ ...todo, completed: !todo.completed })}
+                ></div>
+                <p className={todo.completed ? 'line-through' : ''}>{todo.title}</p>
+              </div>
+              <div className={`flex items-center ${color} ${todo.completed ? 'line-through' : ''}`}>
+                {text}
+              </div>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
